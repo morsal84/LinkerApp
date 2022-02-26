ef2c3706fde077c94fe753a9d4b93dd20f9676d7
mousavi_m <Mousavi_m@samelectronic.com>
2022-02-26 13:07:44 +0330

    Add Link Repository.

---
+++ Linker.Application/Repositories/Link/ILinkRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Linker.Application.Repositories.Link
+{
+    public interface ILinkRepository<T> : IReadRepository<Domain.Entities.Link>
+    {
+        Task<Domain.Entities.Link> GetByShortLinkAsync(string abrevation);
+    }
+}
---
+++ Linker.Persistence/Repositories/Link/LinkRepository.cs
@@ -0,0 +1,25 @@
+﻿using Linker.Application.Repositories.Link;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linker.Persistence.Repositories.Link
+{
+    public class LinkRepository : ReadRepository<Domain.Entities.Link>, ILinkRepository<Domain.Entities.Link>
+    {
+        private readonly LinkerContext _context;
+
+        public LinkRepository(LinkerContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Entities.Link> GetByShortLinkAsync(string abrevation)
+        {
+            return await _context.Links.FirstOrDefaultAsync(c => c.Abrevation == abrevation);
+        }
+    }
+}
