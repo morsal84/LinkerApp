d8fab49f661aef4270b26e0c4609ceea9dffaed7
mousavi_m <Mousavi_m@samelectronic.com>
2022-02-26 12:19:19 +0330

    Add DbContext and link Configurtion.

---
+++ Linker.Persistence/Configuration/LinkConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Linker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linker.Persistence.Configuration
+{
+    public class LinkConfiguration : IEntityTypeConfiguration<Link>
+    {
+        public void Configure(EntityTypeBuilder<Link> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
---
+++ Linker.Persistence/LinkerContext.cs
@@ -0,0 +1,29 @@
+﻿using Linker.Domain.Entities;
+using Linker.Persistence.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linker.Persistence
+{
+    public class LinkerContext : DbContext
+    {
+        public LinkerContext(DbContextOptions<LinkerContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Link> Links { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new LinkConfiguration());
+        }
+    }
+}
