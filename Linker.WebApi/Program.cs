95a200c7508a8efe63009df72a975d035d885eb3
mousavi_m <Mousavi_m@samelectronic.com>
2022-02-26 13:34:35 +0330

    Add DbContext.

--- Linker.WebApi/Program.cs
+++ Linker.WebApi/Program.cs
@@ -1,10 +1,18 @@
+using Linker.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("AppDb");
+builder.Services.AddDbContext<LinkerContext>(x => x.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
